adad2eef2c51b4603c280db2ecef96201da67c3a
jsonencode <magmatic33@gmail.com>
2024-06-09 11:47:21 +0300

    v1

--- app.py
+++ app.py
@@ -1,44 +1,129 @@
-import os
-from pytube import YouTube
-from moviepy.editor import *
-import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
+import pygame
+import time
+import random
 
-def on_progress(stream, chunk, bytes_remaining):
-    total_size = stream.filesize
-    bytes_downloaded = total_size - bytes_remaining
+pygame.init()
 
-    percentage = (bytes_downloaded / total_size) * 100
-    print(f"%{round(percentage, 2)} indirildi.", end="\r")
+# Renkler
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
+purple = (128, 0, 128)
+
+# Oyun ekranı boyutları
+dis_width = 800
+dis_height = 600
+
+# Oyun ekranını oluşturma
+dis = pygame.display.set_mode((dis_width, dis_height))
+pygame.display.set_caption('Yılan Oyunu')
+
+clock = pygame.time.Clock()
+snake_block = 10
+snake_speed = 15
+
+font_style = pygame.font.SysFont("bahnschrift", 25)
+score_font = pygame.font.SysFont("comicsansms", 35)
 
 
-def video_indir(link, kayit_yolu):
-    yt = YouTube(link, on_progress_callback=on_progress)
-    ys = yt.streams.get_highest_resolution()  # en yüksek çözünürlükteki videoyu seçer
-    print("Video indiriliyor...")
-    ys.download(kayit_yolu)
-    print("Video başarıyla indirildi!")
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
 
-def video_to_mp3(video_dosyasi, mp3_dosyasi):
-    video = VideoFileClip(video_dosyasi)
-    video.audio.write_audiofile(mp3_dosyasi)
-    video.audio.close()
-    video.close()
 
-def main():
-    print("Video indirmek için 1'e basın, MP3 dönüştürmek için 2'ye basın.")
-    secim = input("Seçiminizi yapın (1/2): ")
+def your_score(score):
+    value = score_font.render("Skor: " + str(score), True, white)
+    dis.blit(value, [0, 0])
 
-    if secim == "1":
-        link = input("YouTube video linkini girin: ")
-        kayit_yolu = os.getcwd()  # Geçerli çalışma dizinine kaydedilir
-        video_indir(link, kayit_yolu)
-    elif secim == "2":
-        video_dosyasi = input("MP3'e dönüştürülecek video dosyasının yolunu girin (örn: video.mp4): ")
-        mp3_dosyasi = input("Çıktı olarak kaydedilecek MP3 dosyasının adını girin (örn: cikis.mp3): ")
-        video_to_mp3(video_dosyasi, mp3_dosyasi)
-    else:
-        print("Geçersiz seçim!")
 
-if __name__ == "__main__":
-    main()
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    dis.blit(mesg, [dis_width / 6, dis_height / 3])
+
+
+def gameLoop():
+    game_over = False
+    game_close = False
+
+    x1 = dis_width / 2
+    y1 = dis_height / 2
+
+    x1_change = 0
+    y1_change = 0
+
+    snake_List = []
+    Length_of_snake = 1
+
+    foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+    foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+
+    while not game_over:
+
+        while game_close == True:
+            dis.fill(blue)
+            message("Kaybettin! Q-Tekrar oyna veya C-Çık", red)
+            your_score(Length_of_snake - 1)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        gameLoop()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    y1_change = -snake_block
+                    x1_change = 0
+                elif event.key == pygame.K_DOWN:
+                    y1_change = snake_block
+                    x1_change = 0
+
+        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+            game_close = True
+        x1 += x1_change
+        y1 += y1_change
+        dis.fill(blue)
+        pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
+        snake_Head = []
+        snake_Head.append(x1)
+        snake_Head.append(y1)
+        snake_List.append(snake_Head)
+        if len(snake_List) > Length_of_snake:
+            del snake_List[0]
+
+        for x in snake_List[:-1]:
+            if x == snake_Head:
+                game_close = True
+
+        our_snake(snake_block, snake_List)
+        your_score(Length_of_snake - 1)
+
+        pygame.display.update()
+
+        if x1 == foodx and y1 == foody:
+            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+            Length_of_snake += 1
+
+        clock.tick(snake_speed)
+
+    pygame.quit()
+    quit()
+
+
+gameLoop()
